66787ab715c7fa28009973525cad62e5d917b210
Brian Simoni <simonibc@vcu.edu>
2017-10-12 00:43:26 -0400

    Add facebook button

--- wp-content/themes/pubgswag/footer.php
+++ wp-content/themes/pubgswag/footer.php
@@ -4,7 +4,18 @@
 </div> <!-- end wrapper -->
 
 <footer class="footer">
-	<p>content</p>
+    <div id="fb-wrapper">
+        <div class="fb-share-button" data-href="https://pubgswag.com" data-layout="button" data-size="small" data-mobile-iframe="false"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fpubgswag.com%2F&amp;src=sdkpreparse">Share</a></div>
+    </div>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
 </footer>
 
 
