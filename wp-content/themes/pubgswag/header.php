a9c834f98cf7e4ac85a0aecf243d82b5196e1382
Brian Simoni <simonibc@vcu.edu>
2017-10-22 19:30:36 -0400

    Add google tag manager

--- wp-content/themes/pubgswag/header.php
+++ wp-content/themes/pubgswag/header.php
@@ -9,20 +9,23 @@
     <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri() . '/images/favicon-96x96.png'?>">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/images/favicon-16x16.png'?>">
 
-    <!-- Global Site Tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106795185-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
-        gtag('js', new Date());
-
-        gtag('config', 'UA-106795185-1');
-    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5BGBJGQ');</script>
+    <!-- End Google Tag Manager -->
 
     <?php wp_head() ?>
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BGBJGQ"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div class="wrapper">
 
     <nav class="navbar navbar-toggleable-md sticky-top navbar-inverse bg-inverse">
